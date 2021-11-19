1027541099a32f12f3eac8f8a1cea7f3ec45b427
Denys Mazur <93657913+DenysMazur@users.noreply.github.com>
2021-11-19 21:46:06 +0200

    Eslint pritter add to develop (#5) (#6)
    * develop update
    
    * Delete .DS_Store
    
    * Delete client/node_modules directory (#2)
    
    * test
    
    * working
    
    * working
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * final
    
    Co-authored-by: prusvladyslav <79951438+prusvladyslav@users.noreply.github.com>
    Co-authored-by: Denys Mazur <93657913+DenysMazur@users.noreply.github.com>
    
    Co-authored-by: Artem Musiienko <86878003+ArtemMusiienko@users.noreply.github.com>
    Co-authored-by: prusvladyslav <79951438+prusvladyslav@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Button from "@material-ui/core/Button"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = function () {
+    return (
+        <div className="App">
+            <div>
+                Сделал: Eslint Stylelint Pre-commit hook Eslint-config-airbnb
+                Husky MUI
+            </div>
+            <Button variant="contained">Default</Button>
+            <Button variant="contained" color="primary">
+                Primary
+            </Button>
+            <Button variant="contained" color="secondary">
+                Secondary
+            </Button>
+            <Button variant="contained" disabled>
+                Disabled
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                href="#contained-buttons"
+            >
+                Link
+            </Button>
+        </div>
+    )
 }
-
-export default App;
+export default App
