6334629b937ff2abc8dde4af6ede8c9980bcba5f
Artem Musiienko <togethera350@gmail.com>
2021-11-19 16:00:32 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React from "react"
-
-import logo from "./logo.svg"
+import Button from "@material-ui/core/Button"
 import "./App.css"
 
 const App = function () {
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <Button variant="contained">Default</Button>
+            <Button variant="contained" color="primary">
+                Primary
+            </Button>
+            <Button variant="contained" color="secondary">
+                Secondary
+            </Button>
+            <Button variant="contained" disabled>
+                Disabled
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                href="#contained-buttons"
+            >
+                Link
+            </Button>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-import reportWebVitals from "reportWebVitals"
 import React from "react"
 import ReactDOM from "react-dom"
+import reportWebVitals from "./reportWebVitals"
 import "./index.css"
 import App from "./App"
 
