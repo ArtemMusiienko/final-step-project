393deb2beff0e49f29fec24407b7c0c75fd66a89
Artem Musiienko <togethera350@gmail.com>
2021-11-19 14:30:32 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import logo from './logo.svg'
-import './App.css'
-import React from 'react'
+import React from "react"
 
-function App() {
+import logo from "./logo.svg"
+import "./App.css"
+
+const App = function() {
     return (
         <div className="App">
             <header className="App-header">
