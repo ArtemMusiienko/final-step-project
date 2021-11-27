fa558a6c9cf6c832e28127361a4dc476773c441b
Artem Musiienko <a1@1s-MacBook-Air.local>
2021-11-27 22:31:36 +0200

    header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,31 @@
 import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
 import AccountCircle from '@mui/icons-material/AccountCircle'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
 import './Header.scss'
+import { Avatar, Button } from '@mui/material'
+import Container from '@mui/material/Container'
+import { makeStyles } from '@mui/styles'
+import Logo from '../Image/sss.jpg'
 
+const useStyle = makeStyles(theme => {
+  return {
+    root: { flexGrow: 1 },
+    menuButtons: {
+      marginRight: theme.spacing(1)
+    },
+    title: {
+      flexGrow: 1
+    }
+  }
+})
 const Header = () => {
+  const classes = useStyle()
   const [auth, setAuth] = useState(true)
   const [anchorEl, setAnchorEl] = useState(null)
 
@@ -25,53 +39,54 @@ const Header = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <NavLink to="/">Home</NavLink>
-          </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <NavLink to="/cart">Cart</NavLink>
-          </Typography>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <NavLink to="/favorites">Favorites</NavLink>
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right'
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
-            </div>
-          )}
-        </Toolbar>
+      <AppBar position="fixed" color="primary">
+        <Container position="fixed">
+          <Toolbar>
+            <Avatar src={Logo} style={{ width: 35, height: 35 }} className={classes.menuButtons} />
+            <Typography variant="h6" style={{ color: '#46A358' }} className={classes.title}>
+              {' '}
+              GREENSHOP{' '}
+            </Typography>
+            <Box>
+              <Button variant="contained" style={{ backgroundColor: '#46A358', color: '#ffffff' }}>
+                {' '}
+                Login{' '}
+              </Button>
+            </Box>
+            {auth && (
+              <div>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right'
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={handleClose}>My account</MenuItem>
+                </Menu>
+              </div>
+            )}
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   )
