d32dc9362e9346937921621f62c6e26e18ab0946
Artem Musiienko <a1@1s-MacBook-Air.local>
2021-11-28 17:46:57 +0200

    header_footer_add

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,25 +1,84 @@
 import React from 'react'
+import { Avatar } from '@mui/material'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
+import { makeStyles } from '@mui/styles'
+import { flexbox, fontSize, padding, textAlign } from '@mui/system'
+import logo from '../Image/sss.jpg'
 import './Footer.scss'
+import location from '../Image/location.png'
+import mail from '../Image/4213413-email-envelope-gmail-google-latter-mail-message_115415.png'
+import phone from '../Image/phone-handset_icon-icons.com_48252.png'
 
-const Footer = () => (
-  <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Footer
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </Box>
-)
+const useSizes = makeStyles(theme => {
+  return {
+    root: { flexGrow: 1 },
+    menuFooter: {
+      display: 'flex',
+      alignItems: 'center'
+    },
+    forIco: {
+      marginLeft: 23
+    },
+    forContent: {
+      color: 'rgba(61, 61, 61, 1)',
+      fontSize: 14,
+      marginLeft: 60,
+      display: 'flex',
+      alignItems: 'center'
+    }
+  }
+})
+
+const Footer = () => {
+  const sizes = useSizes()
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Box
+          className={sizes.menuFooter}
+          style={{ backgroundColor: 'rgba(70, 163, 88, 0.1)', height: 88 }}
+        >
+          <Box className={sizes.menuFooter}>
+            <Avatar src={logo} style={{ width: 35, height: 35 }} className={sizes.forIco} />
+            <Typography variant="h6" style={{ color: '#46A358', marginLeft: 6, marginTop: 3 }}>
+              {' '}
+              GREENSHOP{' '}
+            </Typography>
+          </Box>
+          <Typography variant="h6" className={sizes.forContent}>
+            <Avatar
+              src={location}
+              variant="square"
+              style={{ width: 30, height: 30, marginRight: 10 }}
+            />
+            70 West Buckingham Ave.
+            <br /> Farmingdale, NY 11735{' '}
+          </Typography>
+          <Typography variant="h6" className={sizes.forContent}>
+            <Avatar
+              src={mail}
+              variant="square"
+              style={{ width: 20, height: 20, marginRight: 10 }}
+            />{' '}
+            contact@greenshop.com{' '}
+          </Typography>
+          <Typography variant="h6" className={sizes.forContent}>
+            <Avatar
+              src={phone}
+              variant="square"
+              style={{ width: 20, height: 20, marginRight: 10 }}
+            />{' '}
+            +88 01911 717 490{' '}
+          </Typography>
+        </Box>
+      </AppBar>
+    </Box>
+  )
+}
 
 export default Footer
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,10 +8,14 @@ import AccountCircle from '@mui/icons-material/AccountCircle'
 import MenuItem from '@mui/material/MenuItem'
 import Menu from '@mui/material/Menu'
 import './Header.scss'
-import { Avatar, Button } from '@mui/material'
+import { borders } from '@mui/system'
+import { Avatar, Button, ImageList, Paper } from '@mui/material'
 import Container from '@mui/material/Container'
+import { NavLink } from 'react-router-dom'
 import { makeStyles } from '@mui/styles'
 import Logo from '../Image/sss.jpg'
+import ico from '../Image/clipart3366677.png'
+import bascet from '../Image/shopping-cart.png'
 
 const useStyle = makeStyles(theme => {
   return {
@@ -21,6 +25,14 @@ const useStyle = makeStyles(theme => {
     },
     title: {
       flexGrow: 1
+    },
+    menuLink: {
+      display: 'flex',
+      justifyContent: 'center'
+    },
+    linkStyle: {
+      textDecoration: 'none',
+      color: '#3D3D3D'
     }
   }
 })
@@ -39,25 +51,65 @@ const Header = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" color="primary">
-        <Container position="fixed">
+      <AppBar position="static" color="primary">
+        <Container position="static">
           <Toolbar>
             <Avatar src={Logo} style={{ width: 35, height: 35 }} className={classes.menuButtons} />
             <Typography variant="h6" style={{ color: '#46A358' }} className={classes.title}>
               {' '}
               GREENSHOP{' '}
             </Typography>
-            <Box>
+            <Box sx={{ flexGrow: 1 }} className={classes.menuLink}>
+              <Typography component="div" sx={{ flexGrow: 1 }} className={classes.menuLink}>
+                <NavLink className={classes.linkStyle} to="/">
+                  Home
+                </NavLink>
+              </Typography>
+              <Typography component="div" sx={{ flexGrow: 1 }}>
+                <NavLink className={classes.linkStyle} to="/cart">
+                  Shop
+                </NavLink>
+              </Typography>
+              <Typography component="div" sx={{ flexGrow: 1 }}>
+                <NavLink className={classes.linkStyle} to="/cart">
+                  {' '}
+                  Plant Care
+                </NavLink>
+              </Typography>
+              <Typography component="div" sx={{ flexGrow: 1 }}>
+                <NavLink className={classes.linkStyle} to="/cart">
+                  Blogs
+                </NavLink>
+              </Typography>
+            </Box>
+            <Box className={classes.menuLink}>
+              <Avatar
+                variant="square"
+                style={{ height: 24, width: 24, marginRight: 33, marginTop: 5 }}
+                src={bascet}
+              />
               <Button variant="contained" style={{ backgroundColor: '#46A358', color: '#ffffff' }}>
                 {' '}
+                <Avatar
+                  variant="square"
+                  src={ico}
+                  style={{ width: 20, height: 20, marginRight: 5 }}
+                />
                 Login{' '}
               </Button>
-              <Button variant="contained" style={{ backgroundColor: '#46A358', color: '#ffffff' }}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: '#46A358', color: '#ffffff', marginLeft: 5 }}
+              >
                 {' '}
-                logout{' '}
+                <Avatar
+                  variant="square"
+                  style={{ width: 30, height: 30, marginRight: 5, backgroundColor: '#46A358' }}
+                />
+                Sign Ap{' '}
               </Button>
             </Box>
-            {auth && (
+            {/* {auth && (
               <div>
                 <IconButton
                   size="large"
@@ -88,7 +140,7 @@ const Header = () => {
                   <MenuItem onClick={handleClose}>My account</MenuItem>
                 </Menu>
               </div>
-            )}
+            )} */}
           </Toolbar>
         </Container>
       </AppBar>
