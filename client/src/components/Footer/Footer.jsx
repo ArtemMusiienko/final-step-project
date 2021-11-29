dc09122ad60a232808e8bdf52a3917eabfe94e44
Artem Musiienko <a1@1s-MacBook-Air.local>
2021-11-29 22:37:19 +0200

    footer done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
-import { Avatar, Paper } from '@mui/material'
-import AppBar from '@mui/material/AppBar'
+import { Avatar, TextField, Button } from '@mui/material'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import { makeStyles } from '@mui/styles'
-import { display } from '@mui/system'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import { flexbox, fontSize, padding, textAlign } from '@mui/system'
 import logo from '../Image/sss.jpg'
 import './Footer.scss'
 import location from '../Image/location.png'
@@ -18,7 +13,10 @@ import faceBook from '../../assets/Facebook.svg'
 import instagram from '../../assets/Instagram.svg'
 import twitter from '../../assets/Twitter.svg'
 import linkid from '../../assets/Linkedin.svg'
-import union from '../../assets/Union.svg
+import union from '../../assets/Union.svg'
+import cactusFirst from '../../assets/cactusfirst.svg'
+import cactusSecond from '../../assets/cactussecond.svg'
+import cactusThird from '../../assets/cactusthird.svg'
 
 const useSizes = makeStyles(theme => {
   return {
@@ -36,7 +34,6 @@ const useSizes = makeStyles(theme => {
       marginLeft: 60,
       display: 'flex',
       alignItems: 'center'
-
     },
     forLastSectio: {
       zIndex: 100,
@@ -51,10 +48,12 @@ const useSizes = makeStyles(theme => {
       marginTop: 16
     },
     forFirsBox: {
-      marginLeft: 23
+      marginLeft: 23,
+      marginTop: 20
     },
     forLastBoxes: {
-      marginLeft: 140
+      marginLeft: 140,
+      marginTop: 20
     }
   }
 })
@@ -63,46 +62,110 @@ const Footer = () => {
   const sizes = useSizes()
   return (
     <>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Box
-            className={sizes.menuFooter}
-            style={{ backgroundColor: 'rgba(70, 163, 88, 0.1)', height: 88 }}
+      <Box
+        style={{
+          height: 250,
+          backgroundColor: '#FBFBFB',
+          display: 'flex',
+          justifyContent: 'space-around'
+        }}
+      >
+        <Box style={{ marginTop: 15, marginBottom: 14 }}>
+          <Avatar variant="square" src={cactusFirst} style={{ width: 130, height: 150 }} />
+          <Typography variant="h6" style={{ fontSize: 17 }}>
+            <b>Garden Care</b>
+          </Typography>
+          <Typography style={{ fontSize: 14 }}>
+            We are an online plant shop offering a <br />
+            wide range of cheap and trendy plants.
+          </Typography>
+        </Box>
+        <Box style={{ marginTop: 15, marginBottom: 14 }}>
+          <Avatar variant="square" src={cactusSecond} style={{ width: 130, height: 150 }} />
+          <Typography variant="h6" style={{ fontSize: 17 }}>
+            <b>Plant Renovation</b>
+          </Typography>
+          <Typography style={{ fontSize: 14 }}>
+            We are an online plant shop offering a<br /> wide range of cheap and trendy plants.
+          </Typography>
+        </Box>
+        <Box style={{ marginTop: 15, marginBottom: 14 }}>
+          <Avatar variant="square" src={cactusThird} style={{ width: 145, height: 150 }} />
+          <Typography variant="h6" style={{ fontSize: 17 }}>
+            <b>Watering Graden</b>
+          </Typography>
+          <Typography style={{ fontSize: 14 }}>
+            We are an online plant shop offering a <br /> wide range of cheap and trendy plants.
+          </Typography>
+        </Box>
+        <Box style={{ marginTop: 15, marginBottom: 14 }}>
+          <Typography variant="h6" style={{ fontSize: 17, marginBottom: 17 }}>
+            {' '}
+            <b>Would you like to join newsletters? </b>
+          </Typography>
+          <TextField variant="outlined" placeholder="Enter your Email" name="msg" id="text" />
+          <Button
+            variant="square"
+            style={{
+              color: '#FFFFFF',
+              height: 56,
+              width: 80,
+              backgroundColor: '#46A358'
+            }}
           >
-            <Box className={sizes.menuFooter}>
-              <Avatar src={logo} style={{ width: 35, height: 35 }} className={sizes.forIco} />
-              <Typography variant="h6" style={{ color: '#46A358', marginLeft: 6, marginTop: 3 }}>
-                {' '}
-                GREENSHOP{' '}
-              </Typography>
-            </Box>
-            <Typography variant="h6" className={sizes.forContent}>
-              <Avatar
-                src={location}
-                variant="square"
-                style={{ width: 30, height: 30, marginRight: 10 }}
-              />
-              70 West Buckingham Ave.
-              <br /> Farmingdale, NY 11735{' '}
-            </Typography>
-            <Typography variant="h6" className={sizes.forContent}>
-              <Avatar
-                src={mail}
-                variant="square"
-                style={{ width: 20, height: 20, marginRight: 10 }}
-              />{' '}
-              contact@greenshop.com{' '}
-            </Typography>
-            <Typography variant="h6" className={sizes.forContent}>
-              <Avatar
-                src={phone}
-                variant="square"
-                style={{ width: 20, height: 20, marginRight: 10 }}
-              />{' '}
-              +88 01911 717 490{' '}
+            Join
+          </Button>
+          <Typography
+            style={{
+              fontSize: 14,
+              marginTop: 18
+            }}
+          >
+            We usually post offers and challenges in newsletter.
+            <br /> We’re your online houseplant destination. <br />
+            We offer a wide range of houseplants and accessories <br />
+            shipped directly from our (green)house to yours!{' '}
+          </Typography>
+        </Box>
+      </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <Box
+          className={sizes.menuFooter}
+          style={{ backgroundColor: 'rgba(70, 163, 88, 0.1)', height: 88 }}
+        >
+          <Box className={sizes.menuFooter}>
+            <Avatar src={logo} style={{ width: 35, height: 35 }} className={sizes.forIco} />
+            <Typography variant="h6" style={{ color: '#46A358', marginLeft: 6, marginTop: 3 }}>
+              {' '}
+              GREENSHOP{' '}
             </Typography>
           </Box>
-        </AppBar>
+          <Typography variant="h6" className={sizes.forContent}>
+            <Avatar
+              src={location}
+              variant="square"
+              style={{ width: 30, height: 30, marginRight: 10 }}
+            />
+            70 West Buckingham Ave.
+            <br /> Farmingdale, NY 11735{' '}
+          </Typography>
+          <Typography variant="h6" className={sizes.forContent}>
+            <Avatar
+              src={mail}
+              variant="square"
+              style={{ width: 20, height: 20, marginRight: 10 }}
+            />{' '}
+            contact@greenshop.com{' '}
+          </Typography>
+          <Typography variant="h6" className={sizes.forContent}>
+            <Avatar
+              src={phone}
+              variant="square"
+              style={{ width: 20, height: 20, marginRight: 10 }}
+            />{' '}
+            +88 01911 717 490{' '}
+          </Typography>
+        </Box>
       </Box>
       <Box className={sizes.forLastSectio}>
         <Box className={sizes.forFirsBox}>
@@ -253,48 +316,10 @@ const Footer = () => {
           />
         </Box>
       </Box>
+      <Box style={{ display: 'flex', justifyContent: 'center', marginTop: 8 }}>
+        <Typography>© 2021 GreenShop. All Rights Reserved.</Typography>
+      </Box>
     </>
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Box
-          className={sizes.menuFooter}
-          style={{ backgroundColor: 'rgba(70, 163, 88, 0.1)', height: 88 }}
-        >
-          <Box className={sizes.menuFooter}>
-            <Avatar src={logo} style={{ width: 35, height: 35 }} className={sizes.forIco} />
-            <Typography variant="h6" style={{ color: '#46A358', marginLeft: 6, marginTop: 3 }}>
-              {' '}
-              GREENSHOP{' '}
-            </Typography>
-          </Box>
-          <Typography variant="h6" className={sizes.forContent}>
-            <Avatar
-              src={location}
-              variant="square"
-              style={{ width: 30, height: 30, marginRight: 10 }}
-            />
-            70 West Buckingham Ave.
-            <br /> Farmingdale, NY 11735{' '}
-          </Typography>
-          <Typography variant="h6" className={sizes.forContent}>
-            <Avatar
-              src={mail}
-              variant="square"
-              style={{ width: 20, height: 20, marginRight: 10 }}
-            />{' '}
-            contact@greenshop.com{' '}
-          </Typography>
-          <Typography variant="h6" className={sizes.forContent}>
-            <Avatar
-              src={phone}
-              variant="square"
-              style={{ width: 20, height: 20, marginRight: 10 }}
-            />{' '}
-            +88 01911 717 490{' '}
-          </Typography>
-        </Box>
-      </AppBar>
-    </Box>
   )
 }
 
