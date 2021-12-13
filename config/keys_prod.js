4b3524cf1a0fbf1e8a84ff1e3ee9ff55801f985b
Artem Musiienko <a1@1s-MacBook-Air.local>
2021-12-14 00:07:38 +0200

    config

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY,
-  nodemailerUser: process.env.NODEMAILER_USER,
-  nodemailerPassword: process.env.NODEMAILER_PASSWORD,
-  nodemailerService: process.env.NODEMAILER_SERVICE
+  mongoURI = mongodb+srv://admin:qYeJ3qc5gaipufoC@cluster0.yhtuy.mongodb.net/app?retryWrites=true&w=majority
+  secretOrKey=secret,
+  nodemailerUser=finalprojectfe2@gmail.com,
+  nodemailerPassword=VVADR2021$q,
+  nodemailerService=gmail
 };
