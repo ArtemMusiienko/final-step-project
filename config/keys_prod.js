a698dc065b8d63d3b5e77862f0617d5fadb03803
Artem Musiienko <togethera350@gmail.com>
2021-12-14 14:21:20 +0200

    test

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,7 +1,8 @@
 module.exports = {
-  mongoURI = "mongodb+srv://admin:qYeJ3qc5gaipufoC@cluster0.yhtuy.mongodb.net/app?retryWrites=true&w=majority",
-  secretOrKey=secret,
-  nodemailerUser=finalprojectfe2@gmail.com,
-  nodemailerPassword=VVADR2021$q,
-  nodemailerService=gmail
-};
+  mongoURI:
+    'mongodb+srv://admin:qYeJ3qc5gaipufoC@cluster0.yhtuy.mongodb.net/app?retryWrites=true&w=majority',
+  secretOrKey: 'secret',
+  nodemailerUser: 'finalprojectfe2@gmail.com',
+  nodemailerPassword: 'VVADR2021$q',
+  nodemailerService: 'gmail'
+}
