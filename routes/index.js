82ed08e1e92f9443829bf018b6944d5fad66a17c
Artem Musiienko <togethera350@gmail.com>
2021-12-14 16:07:25 +0200

    tsst

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,8 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build"));
+});
 
 module.exports = router;
