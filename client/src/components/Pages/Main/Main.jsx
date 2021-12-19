462ff302730e87c50e4e78853a51484aebe9b813
prusvladyslav <79951438+prusvladyslav@users.noreply.github.com>
2021-12-19 23:17:55 +0200

    fix (#63)
    * Adapted,categories rework,cards rework etc
    
    * fix
    
    Co-authored-by: Prus Vladyslav <vlpr@opinov8.com>

--- client/src/components/Pages/Main/Main.jsx
+++ client/src/components/Pages/Main/Main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useSelector } from 'react'
 import './Main.scss'
 
-const Main = () => {
-  const { count } = useSelector(state => state.counter)
-  const { increment, decrement } = counterSlice.actions
-  const dispatch = useDispatch()
+
+const Main = () => (
+  // const { count } = useSelector(state => state.counter)
+  // const { increment, decrement } = counterSlice.actions
+  // const dispatch = useDispatch()
   return (
-    <div>
+
       {/* <div>{count}</div>
+
       <Button variant="contained" color="primary" onClick={() => dispatch(increment())}>
         increment counter
       </Button>
@@ -17,9 +19,11 @@ const Main = () => {
       <Button variant="contained" color="secondary" onClick={() => dispatch(asyncIncrement())}>
         Async increment counter
       </Button> */}
-    </div>
-  )
+
+)
+
 }
+
 
 
 export default Main
