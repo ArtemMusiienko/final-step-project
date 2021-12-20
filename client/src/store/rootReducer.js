98dc5217531b377d0b5039ddf6fd80a5705f359c
Artem Musiienko <togethera350@gmail.com>
2021-12-20 19:37:15 +0200

    basket

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit'
 import authReducer from './auth/reducer'
 import messageReducer from './message/reducer'
 import catalogReducer from './catalog/reducer'
+import basketReducer from './buscet/buscetSlise'
 
 const rootReducer = combineReducers({
   auth: authReducer,
   message: messageReducer,
-  catalog: catalogReducer
+  catalog: catalogReducer,
+  basket: basketReducer
 })
 
 export default rootReducer
