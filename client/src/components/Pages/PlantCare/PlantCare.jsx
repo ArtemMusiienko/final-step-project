8a0c2857c50d70c70b58599bc30723be169e9266
VitaliiBaz00 <87306646+VitaliiBaz00@users.noreply.github.com>
2021-12-21 19:55:25 +0200

    fix plant care

--- client/src/components/Pages/PlantCare/PlantCare.jsx
+++ client/src/components/Pages/PlantCare/PlantCare.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './PlantCare.scss'
-import { Container, Typography } from '@mui/material'
-import { makeStyles } from '@mui/styles'
+import {Container, Typography} from '@mui/material'
+import {makeStyles} from '@mui/styles'
 import image from '../../../assets/image/plant-care.png'
+import Box from "@mui/material/Box";
 
 const useStyle = makeStyles(theme => {
   return {
@@ -22,17 +23,17 @@ const PlantCare = () => {
 
   return (
     <Container className="plant-care">
-      <div className="plant-care__title-wrapper">
+      <Box className="plant-care__title-wrapper">
         <Typography
           variant="h3"
-          style={{ marginTop: '2rem', marginBottom: '2rem', color: '#46A358' }}
+          style={{marginTop: '2rem', marginBottom: '2rem', color: '#46A358'}}
           className="plant-care__title"
         >
           Our Top 7 Plant Care Tips
         </Typography>
         <Typography
           variant="h4"
-          style={{ textAlign: 'center', marginBottom: '3rem' }}
+          style={{textAlign: 'center', marginBottom: '3rem'}}
           className="plant-care__title-description"
         >
           There are plenty of ways to care for your plant. With the help of our plant experts, we’ve
@@ -41,11 +42,11 @@ const PlantCare = () => {
         <img
           src={image}
           alt=""
-          style={{ height: '100%', width: '100%' }}
+          style={{height: '100%', width: '100%'}}
           className="plant-care__title-img"
         />
-      </div>
-      <div className="plant-care__article-wrapper">
+      </Box>
+      <Box className="plant-care__article-wrapper">
         <Typography variant="h5" className={classes.articleTitle}>
           1.Choose plants based on your light{' '}
         </Typography>
@@ -110,50 +111,54 @@ const PlantCare = () => {
         <Typography variant="h5" className={classes.articleTitle}>
           4.Raise humidity levels when needed{' '}
         </Typography>
-        <p className={classes.articleText}>
+        <Typography className={classes.articleText}>
           Staying true to your plant&aposs; natural environment will help your plant thrive indoors.
           Most tropical plants prefer high humidity and bright to moderate, indirect light. During
           the dry months of winter, grouping similar plants together helps to create a more humid
-        <Typography className={classes.articleText}>
-          Staying true to your plant's natural environment will help your plant thrive indoors. Most
-          tropical plants prefer high humidity and bright to moderate, indirect light. During the
-          dry months of winter, grouping similar plants together helps to create a more humid
-          microclimate. A humidifier can help too and it’s great for humans (find more ways to
-          increase humidity levels here). On the other hand, most desert dwellers like cacti and
-          succulents prefer dry air and bright, direct light with no shade at all. They don’t much
-          care for humidity.
         </Typography>
-        <Typography variant="h5" className={classes.articleTitle}>
-          5.Always keep temperatures stable{' '}
-        </Typography>
-        <Typography className={classes.articleText}>
-          Keep your plant’s home environment as stable as possible. Extreme changes can stress
-          plants out. Keep the temperature between 65 and 85 degrees F, and avoid placing your
-          plants near radiators, A/C units, and forced-air vents, which can create hot or cold
-          drafts.
-        </Typography>
-        <Typography variant="h5" className={classes.articleTitle}>
-          6.Know when to skip the fertilizer
-        </Typography>
-        <Typography className={classes.articleText}>
-          Be mindful when using fertilizer on your houseplants. Too much fertilizer can do more harm
-          than good. Houseplants tend to not need fertilizer as often as outdoor plants do. If you
-          do choose to fertilize your plant, it’s best to do so during the growing season (early
-          spring to early fall) and follow the general rule of thumb: ‘less is more’. Most
-          store-bought fertilizers should be diluted with water before use.
-        </Typography>
-        <Typography variant="h5" className={classes.articleTitle}>
-          7.Shop from a reliable source{' '}
-        </Typography>
-        <Typography className={classes.articleText}>
-          Be mindful when using fertilizer on your houseplants. Too much fertilizer can do more harm
-          than good. Houseplants tend to not need fertilizer as often as outdoor plants do. If you
-          do choose to fertilize your plant, its best to do so during the growing season (early
-          spring to early fall) and follow the general rule of thumb: ‘less is more’. Most
-          store-bought fertilizers should be diluted with water before use.
-        </Typography>
-      </div>
+          <Typography className={classes.articleText}>
+            Staying true to your plant's natural environment will help your plant thrive indoors.
+            Most
+            tropical plants prefer high humidity and bright to moderate, indirect light. During the
+            dry months of winter, grouping similar plants together helps to create a more humid
+            microclimate. A humidifier can help too and it’s great for humans (find more ways to
+            increase humidity levels here). On the other hand, most desert dwellers like cacti and
+            succulents prefer dry air and bright, direct light with no shade at all. They don’t much
+            care for humidity.
+          </Typography>
+          <Typography variant="h5" className={classes.articleTitle}>
+            5.Always keep temperatures stable{' '}
+          </Typography>
+          <Typography className={classes.articleText}>
+            Keep your plant’s home environment as stable as possible. Extreme changes can stress
+            plants out. Keep the temperature between 65 and 85 degrees F, and avoid placing your
+            plants near radiators, A/C units, and forced-air vents, which can create hot or cold
+            drafts.
+          </Typography>
+          <Typography variant="h5" className={classes.articleTitle}>
+            6.Know when to skip the fertilizer
+          </Typography>
+          <Typography className={classes.articleText}>
+            Be mindful when using fertilizer on your houseplants. Too much fertilizer can do more
+            harm
+            than good. Houseplants tend to not need fertilizer as often as outdoor plants do. If you
+            do choose to fertilize your plant, it’s best to do so during the growing season (early
+            spring to early fall) and follow the general rule of thumb: ‘less is more’. Most
+            store-bought fertilizers should be diluted with water before use.
+          </Typography>
+          <Typography variant="h5" className={classes.articleTitle}>
+            7.Shop from a reliable source{' '}
+          </Typography>
+          <Typography className={classes.articleText}>
+            Be mindful when using fertilizer on your houseplants. Too much fertilizer can do more
+            harm
+            than good. Houseplants tend to not need fertilizer as often as outdoor plants do. If you
+            do choose to fertilize your plant, its best to do so during the growing season (early
+            spring to early fall) and follow the general rule of thumb: ‘less is more’. Most
+            store-bought fertilizers should be diluted with water before use.
+          </Typography>
+      </Box>
     </Container>
-  )
+)
 }
 export default PlantCare
