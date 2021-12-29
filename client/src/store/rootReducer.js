bbfb221bc1eff7f98ab35de003574d981cdbf031
Artem Musiienko <86878003+ArtemMusiienko@users.noreply.github.com>
2021-12-29 19:54:50 +0200

    cart (#86)

--- client/src/store/rootReducer.js
+++ client/src/store/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from '@reduxjs/toolkit'
 import authReducer from './auth/reducer'
 import messageReducer from './message/reducer'
 import catalogReducer from './catalog/reducer'
-import basketReducer from './basket/basketSlise'
+import cartReducer from './cart/basketSlise'
 import productReducer from './products/reducer'
 import wishlistReducer from './wishlist/reducer'
 
@@ -10,7 +10,7 @@ const rootReducer = combineReducers({
   auth: authReducer,
   message: messageReducer,
   catalog: catalogReducer,
-  basket: basketReducer,
+  cart: cartReducer,
   products: productReducer,
   wishlist: wishlistReducer
 })
