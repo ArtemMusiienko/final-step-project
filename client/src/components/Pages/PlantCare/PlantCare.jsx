82b306e69841bce973480063098d3e0d640e5c42
Prus Vladyslav <vlpr@opinov8.com>
2022-01-11 23:44:02 +0200

    fix

--- client/src/components/Pages/PlantCare/PlantCare.jsx
+++ client/src/components/Pages/PlantCare/PlantCare.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './PlantCare.scss'
 import { Avatar, Button, Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import { useNavigate } from 'react-router-dom'
